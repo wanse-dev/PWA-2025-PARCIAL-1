f468e938d6d104658fd5f237849481240603d956
wanse-dev <wansedev@gmail.com>
2025-06-02 19:42:19 -0300

    added default port value, in case .env port value doesn't work

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import routes from "./routes/index";
 import connectDB from "./database";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
